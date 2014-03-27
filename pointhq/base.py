d6bfe5d24c9dabd5367c7f7f6e3fd6851482fd40
Ilya Kogan <ikogan@flarecode.com>
2014-03-27 02:38:42 -0400

    PointHQ returns a 202 on an update call, not a 200
    ikogan@Icarus:~/Documents/Code/devops/servers$ curl -i -u "${CREDENTIALS}" -H 'Accept: application/json' -H "Content-type: application/json" https://pointhq.com/zones/${zoneid}/records/${recordid} -X PUT -d '{"zone_record":{"data":"1.2.3.5"}}'
    HTTP/1.1 202 Accepted
    Server: nginx/1.4.1
    Date: Thu, 27 Mar 2014 06:35:50 GMT
    Content-Type: application/json
    Transfer-Encoding: chunked
    Connection: keep-alive
    Status: 202 Accepted
    X-UA-Compatible: IE=Edge,chrome=1
    Cache-Control: no-cache
    X-Runtime: 0.022793
    X-Rack-Cache: invalidate, pass
    
    {"zone_record":{"name":"${name}","data":"1.2.3.5","id":${recordid},"aux":null,"record_type":"A","redirect_to":null,"ttl":3600,"zone_id":${zoneid}}}

--- pointhq/base.py
+++ pointhq/base.py
@@ -36,7 +36,7 @@ class BaseMember(BaseResourse):
 
     def update(self, **kwargs):
         response = self.request('put', kwargs)
-        if response.status == 200:
+        if response.status == 202:
             return
         if response.status == 422:
             raise exceptions.UnprocessableEntityError(json.loads(response.content))
